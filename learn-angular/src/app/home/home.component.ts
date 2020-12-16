0634c1b5a4f898aaaecb3152fef289ccc3f126e1
jakkratornpha <jakkratorn.phanchaweng@allianz.com>
2020-12-16 11:50:38 +0700

    cahange directory

--- learn-angular/src/app/home/home.component.ts
+++ learn-angular/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  title: string
+  
+  constructor() {
+    console.log(`[Constructor] - title value = ${this.title}`)
+   }
 
   ngOnInit(): void {
+    console.log(`[OnInit] - title value = ${this.title}`)
   }
 
 }
